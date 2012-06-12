6f21a4bf6c456b921ca88053c1c911cef31c4818
Sheena McNeil <mcneis@rpi.edu>
2012-06-12 09:43:19 -0400

    changed to python 3

--- database.py
+++ database.py
@@ -15,31 +15,32 @@ except:
 def Add_to_db():
 	keep_adding = True
 	while keep_adding == True:
-		recipe = raw_input('Recipe name: ').lower()
+		recipe = input('Recipe name: ').lower()
 		flag = False
 		for row in cur.execute("select * from recipes"):
 			if row[0] == recipe:
 				flag = True
-				print 'Recipe is already in database'
+				print ('Recipe is already in database')
 
-		meat = raw_input('type of meat: ').lower()
-		num_veggies = raw_input('number of vegetables (up to four): ')
+		meat = input('type of meat: ').lower()
+		num_veggies = input('number of vegetables (up to four): ')
 		VEGGIES = []
 		for x in range(0, int(num_veggies)):
-			veggie = raw_input('veggie: ').lower()
+			veggie = input('veggie: ').lower()
 			VEGGIES.append(veggie)
 
 		if int(num_veggies) < 4:
 			for x in range(int(num_veggies), 4):
 				VEGGIES.append('none')
 
+		print (VEGGIES)
 		if flag == False:
-			in_dir = raw_input('Is the recipe file in the current directory and a *.txt file? (y/n)')
+			in_dir = input('Is the recipe file in the current directory and a *.txt file? (y/n)')
 			if in_dir =='y':
 				full_path = recipe + '.txt'
 
 			elif in_dir == 'n':
-				file_path = raw_input('Recipe file to acces by path:  ')
+				file_path = input('Recipe file to acces by path:  ')
 				full_path = os.path.abspath(file_path)
 			
 			g = open(full_path)
@@ -48,30 +49,30 @@ def Add_to_db():
 			cur.execute("insert into recipes values (?,?,?,?,?,?,?)", (recipe, meat, VEGGIES[0], VEGGIES[1], VEGGIES[2], VEGGIES[3], recipe_string))
 		conn.commit()
 
-		go_on = raw_input('Keep adding? (y/n): ').lower()
+		go_on = input('Keep adding? (y/n): ').lower()
 		if go_on == 'n':
 			keep_adding = False
 
 def Delete_from_db():
 	keep_deleting = True
 	while keep_deleting == True:
-		to_be_deleted = raw_input("Recipe name to delete: ").lower()
+		to_be_deleted = input("Recipe name to delete: ").lower()
 		query = "delete from recipes where recipe_name = '%s'" % to_be_deleted
 		cur.execute(query)
 		conn.commit()
 	
-		go_on = raw_input('Keep deleting? (y/n): ')
+		go_on = input('Keep deleting? (y/n): ')
 		if go_on == 'n':
 			keep_deleting = False
 
 def Print_db():
 	for row in cur.execute("select * from recipes"):
-		print '-'*10
-		print 'Recipe:', row[0]
-		print 'Meat:', row[1]
-	for x in range(2, 6):
-		if row[x] != 'none':
-			print 'Veggie', (x-1), ':', row[x] 
+		print ('-'*10)
+		print ('Recipe:', row[0])
+		print ('Meat:', row[1])
+		for x in range(2, 6):
+			if row[x] != 'none':
+				print ('Veggie', (x-1), ':', row[x] )
 
 def Find_recipe(MEAT, VEGGIES, NUM):
 	qualifying_recipes = []
@@ -90,4 +91,4 @@ def Print_recipe(RECIPE):
 	RECIPE = RECIPE.lower()
 	query = "select * from recipes where recipe_name = '%s'" % RECIPE
 	for row in cur.execute(query):
-		print row[6]
+		print (row[6])
--- koobkooc.py
+++ koobkooc.py
@@ -5,7 +5,7 @@ import os
 import database
 #--------Database Functions--------#
 def DB():
-	db_instruction = raw_input('DATABASE INSTRUCTION: ')
+	db_instruction = input('DATABASE INSTRUCTION: ')
 	if db_instruction == 'add':
 		database.Add_to_db()
 	elif db_instruction == 'delete':
@@ -15,21 +15,21 @@ def DB():
 
 #--------Recipe Functions/Finding--------#
 def RECIPES():
-	meat = raw_input("What type of meat would you like to use? ")
-	num_veggies = raw_input("How many vegetables would you like to use? ")
+	meat = input("What type of meat would you like to use? ")
+	num_veggies = input("How many vegetables would you like to use? ")
 	VEGGIES = []
 	for x in range(0, int(num_veggies)):
-		veggie = raw_input("veggie: ").lower()
+		veggie = input("veggie: ").lower()
 		VEGGIES.append(veggie)
 
 	stop = False
 	while stop == False:
 		recipes = database.Find_recipe(meat, VEGGIES, int(num_veggies))
 		for x in recipes:
-			print x
-		choose_recipe = raw_input("Which recipe would you like to view? ")
+			print (x)
+		choose_recipe = input("Which recipe would you like to view? ")
 		database.Print_recipe(choose_recipe)
-		view_other =  raw_input('\n\nWould you like to go back and view a different recipe? (y/n)')
+		view_other =  input('\n\nWould you like to go back and view a different recipe? (y/n)')
 		if view_other == 'y':
 			os.system('clear')
 		if view_other == 'n':
@@ -40,16 +40,16 @@ print ('Welcome to koobkooc---a-reverse-cookbook')
 print ('OPTIONS: find a recipe or modify the database')
 keep_going = True
 while keep_going == True:
-	instruction = raw_input('INSTRUCTION: ')
+	instruction = input('INSTRUCTION: ')
 	if instruction != 'database' and instruction != 'recipes':
-		print 'USAGE: "recipes" or "database"'
-		instruction  = raw_input('INSTRUCTION: ')
+		print ('USAGE: "recipes" or "database"')
+		instruction  = input('INSTRUCTION: ')
 
 	if instruction == 'database':
 		DB()
 	if instruction == 'recipes':
 		RECIPES()
-	go_on = raw_input('Do you want to do something else? (y/n): ')
+	go_on = input('Do you want to do something else? (y/n): ')
 	if go_on == 'n':
 		keep_going = False
 
